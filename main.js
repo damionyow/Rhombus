ee6b99bc32a1880e2765b6851b382abc959b05cb
damionyow <97314370+damionyow@users.noreply.github.com>
2022-02-05 13:11:06 -0800

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,41 @@
 // var symbol ="*";
 
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
+upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
 upRight(pHeight, pColorEven, pColorOdd, pSymbol);
 downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+	
+}
+
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=1;i<=pHeight;i++){
+rLine +="<p>";
+	
+//Fill in the left side of every row with spaces or blanks
+for (x=1; x<=pHeight-i;x++){
+	rLine +="-;
+}
+	
+//Create each line on the Rhombus
+for(j=0;j<=pHeight;j++){
+
+//Is the position even or odd so we change the color
+if (j%2)
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+// console.log(rLine);
+
+}
+
+document.getElementById("upLeft").innerHTML = rLine;
+}
 
 }
 
